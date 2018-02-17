6831dfdacbe486fd03e18aa3ff1dfbaaa1c1f256
cmilkaitis <chris.milkaitis@gmail.com>
2018-02-16 22:10:06 -0500

    loops2

---
+++ loops2.js
@@ -0,0 +1,84 @@
+
+'use strict';
+
+function average(numbers) {
+  let sum = 0;
+ 
+        
+  numbers.forEach(function(num) {
+    sum += num;
+    
+  });
+  return sum / numbers.length;
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// tests
+  
+function testFunctionWorks(fn, input, expected) {
+  if (fn(input) === expected) {
+    console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
+    return true;
+  } else {
+    console.log(
+      'FAILURE: `' +
+          fn.name +
+          '([' +
+          input +
+          '])` should be ' +
+          expected +
+          ' but was ' +
+          fn(input)
+    );
+    return false;
+  }
+}
+  
+(function runTests() {
+  const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const correctAns1 = 5.5;
+  const numList2 = [0, -1, 1];
+  const correctAns2 = 0;
+  
+  const testResults = [
+    testFunctionWorks(average, numList1, correctAns1),
+    testFunctionWorks(average, numList2, correctAns2),
+  ];
+  const numPassing = testResults.filter(function(result) {
+    return result;
+  }).length;
+  console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
+})();
