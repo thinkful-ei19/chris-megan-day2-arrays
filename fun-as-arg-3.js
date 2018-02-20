28a6ce1efb864ff93ec4eab21f6ddb00c4dc1ad1
leonardom249 <leonardo.megan@gmail.com>
2018-02-20 14:38:45 -0600

    final fun-as-arg-3.js

---
+++ fun-as-arg-3.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function hazardWarningCreator(typeOfWarning){
+  let warningCounter=0;
+  return function(location){
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}`);
+    console.log (`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  };
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Main St and Pacific Ave');
